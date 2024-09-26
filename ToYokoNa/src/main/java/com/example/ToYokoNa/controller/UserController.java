1f8e4cfd5e019523e26e29aa2e431c50d4449dc3
murayama_misato <murayama.misato@alhinc.com>
2024-09-26 16:39:49 +0900

    管理者権限フィルターの実装とユーザー管理画面ファイルの用意まで

--- ToYokoNa/src/main/java/com/example/ToYokoNa/controller/UserController.java
+++ ToYokoNa/src/main/java/com/example/ToYokoNa/controller/UserController.java
@@ -1,23 +1,17 @@
 package com.example.ToYokoNa.controller;
 
 import com.example.ToYokoNa.controller.form.UserForm;
-import com.example.ToYokoNa.repository.entity.User;
 import com.example.ToYokoNa.service.UserService;
 import com.example.ToYokoNa.utils.CipherUtil;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class UserController {
@@ -70,5 +64,13 @@ public class UserController {
         // topにリダイレクト
         mav.setViewName("redirect:/");
         return mav;
+    }
+
+    /*
+     * ユーザー管理画面表示
+     */
+    @GetMapping("/userManage")
+    public ModelAndView userManage() {
+        return new ModelAndView("userManage");
     }
 }
