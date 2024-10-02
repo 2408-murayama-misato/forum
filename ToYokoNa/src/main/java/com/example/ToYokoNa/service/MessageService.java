a760b7522750c06d3fc1c8a1729cdd28051dd60c
tokunaga_taishu <tokunaga.taishu@alhinc.jp>
2024-10-02 09:19:57 +0900

    空白行修正

--- ToYokoNa/src/main/java/com/example/ToYokoNa/service/MessageService.java
+++ ToYokoNa/src/main/java/com/example/ToYokoNa/service/MessageService.java
@@ -8,6 +8,7 @@ import com.example.ToYokoNa.repository.entity.Message;
 import com.example.ToYokoNa.repository.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
